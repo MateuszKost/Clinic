f04c716cc911f638246251e08c8fa035048ef736
Oskar <reoskaro434@gmail.com>
2021-09-14 22:50:28 +0200

    Refactored code, improved layout

--- front/clinic/src/app/patient/patient.component.ts
+++ front/clinic/src/app/patient/patient.component.ts
@@ -1,4 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export interface PeriodicElement {
+  id: number;
+  name: string;
+  surname: string;
+}
+
+const DATA: PeriodicElement[] = [
+  { id: 1, name: 'Andrzej', surname: 'Jaworek' },
+  { id: 2, name: 'Zbigniew', surname: 'Stonoga' },
+  { id: 3, name: 'Mareczek', surname: 'Heheczek' },
+  { id: 4, name: 'Mateusz', surname: 'Szary' },
+  { id: 5, name: 'Jas', surname: 'Fasola' },
+  { id: 6, name: 'Anielka', surname: 'Mirabelka' },
+
+];
 
 @Component({
   selector: 'app-patient',
@@ -7,9 +24,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['demo-id', 'demo-name', 'demo-surname'];
+  title = 'clinic';
+  dataSource = DATA;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+
   }
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onClick = () => {
+    console.log("test button clicked")
+    const response = this.http.get("http://localhost:8080/clinic/test/123", { responseType: 'json' }).subscribe((res) => console.log(res))
+    console.log(response)
+  }
+
+  onRowClicked = (id: Number) => {
+    console.log(id)
+  }
 }
